d434436cfee0ac46f124d25edb0aa0b07645b55c
91703 <91703@NIKITHA>
2023-07-09 19:24:08 +0530

    Third commit

--- TestProject/src/Calculator.java
+++ TestProject/src/Calculator.java
@@ -2,6 +2,10 @@
 public class Calculator {
 	
 	int n1=5, n2=2;
+	
+	public void product() {
+		System.out.println(n1*n2);
+	}
 
 	 public void add() {
 		 System.out.println("Sum is : " +(n1+n2));	 }
@@ -10,6 +14,7 @@ public class Calculator {
 		
      Calculator c = new Calculator();
      c.add();
+     c.product();
 	}
 
 }
